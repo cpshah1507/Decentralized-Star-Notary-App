9c425787a26b6eec86adffa0c93b6ea0740dcdbf
Shah <CShah@opex.local>
2018-12-17 18:50:12 +0000

    Deploying contract to rinkeby

--- smart_contracts/truffle-config.js
+++ smart_contracts/truffle-config.js
@@ -11,6 +11,8 @@
  *     gasPrice: 10000000000,
  *   },
  */
+const HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = '';
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -20,6 +22,14 @@ module.exports = {
         host: "localhost",
         port: 7545,
         network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: function() { 
+        return new HDWalletProvider(mnemonic, 'URL HERE') 
+      },
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000,
     }
   }
 };
